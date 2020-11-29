632509c99debac220005769dd528e042bd86a4d9
Matleyx78 <matmat78@hotmail.it>
2020-11-29 19:09:00 +0100

    Create url_helper.php

---
+++ src/Helpers/url_helper.php
@@ -0,0 +1,14 @@
+<?php
+    
+    function mt_str_to_url($stringa)
+        {
+            $urlstyle = filter_var($stringa, FILTER_SANITIZE_ENCODED);
+            //$urlstyle = urlencode($stringa);
+            return $urlstyle;
+        }
+
+    function mt_url_to_str($stringa)
+        {
+            $rightstyle = urldecode($stringa);
+            return $rightstyle;
+        }
