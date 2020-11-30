d7d89f5623fa04a6b3f3a87057dc7a6f370dbceb
Matleyx78 <matmat78@hotmail.it>
2020-11-30 10:28:09 +0100

    Delete test_2.php

--- app/Views/test_2.php
+++
@@ -1,6 +0,0 @@
-Test 2 nessun post
-<form action="test/send" method="post">
-    <input type="text" name="name" value="Mario" id="name">
-    <input type="text" name="lastname" value="Rossi" id="lastname">
-    <input type="submit" name="submit" value="submit" id="submit">
-</form>
