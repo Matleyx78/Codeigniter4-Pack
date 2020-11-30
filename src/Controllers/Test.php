486b773221d543b6a83699e89d0f30724aa078b6
Matleyx78 <matmat78@hotmail.it>
2020-11-30 10:15:14 +0100

    Create Test.php

---
+++ src/Controllers/Test.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controllers;
+
+use Matleyx\CI4P\Libraries\Calendario;
+
+class Test extends BaseController
+{
+    // available to GET requests only
+    public function index()
+    {
+		helper('date');
+		$cal = new Calendario();
+		if ( $this->ionAuth->loggedIn() )
+			{
+				$data['ris'] = 'si';
+			}
+			else
+			{
+				$data['ris'] = 'no';}
+		$data['giorno1'] = now();
+		$data['mysql'] = $cal->primo_del_mese();
+		$data['ris2'] = mysql_to_human1($data['mysql']);
+		
+        return view('test/test',$data);
+    }
+
+} 
