a26b03642e8051b9fe24aa520bf278b5e1facbe9
Matleyx78 <matmat78@hotmail.it>
2020-12-03 09:37:04 +0100

    Update

--- app/Views/test/testview.php
+++ app/Views/test/testview.php
@@ -1,5 +1,5 @@
 <?php $this->extend('layouts/main'); ?>
 
 <?php $this->section('content'); ?>
-<h1><?php echo 'sticazzi '.$ris.' ecco'; ?></h1>;
+<h1><?php echo 'sticazzi '.$ris.' ecco'; ?></h1>
 <?php $this->endSection(); ?>
