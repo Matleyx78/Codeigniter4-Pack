59353c6781b987c77edff06bc8a1c15a67460e4a
matteo.lenzi <matteo.lenzi@NB-Matteo.mshome.net>
2020-12-05 19:24:53 +0100

    Update

---
+++ src/Controllers/Sticazzi.php
@@ -0,0 +1,17 @@
+<?php namespace Matleyx\CI4P\Controllers;
+
+use App\Controllers\BaseController;
+
+class Sticazzi extends BaseController
+{
+        public function __construct() {
+   
+
+
+        }
+	public function index()
+	{
+		echo 'controller sticazzi';
+	}
+
+}
