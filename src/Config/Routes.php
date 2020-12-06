b9fde6022366d2548bb8ef93bb5028d8652c8595
Matleyx78 <matmat78@hotmail.it>
2020-12-06 19:13:15 +0100

    Update Routes.php

--- src/Config/Routes.php
+++ src/Config/Routes.php
@@ -3,6 +3,6 @@
 $routes->group('mt_crud', ['namespace' => 'Matleyx\CI4P\Controllers'], function ($routes)
 {
 	$routes->get('/', 'Mt_crud::index');
-	$routes->get('crudgen', 'Mt_crud::crudgen');
+	$routes->add('crudgen', 'Mt_crud::crudgen');
 
 });
