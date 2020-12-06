9f1a200699c34c9bbb381bf1dbe051cf77d26854
matteo.lenzi <matteo.lenzi@NB-Matteo.mshome.net>
2020-12-06 13:39:47 +0100

    Update

--- src/Controllers/Mt_crud.php
+++ src/Controllers/Mt_crud.php
@@ -8,17 +8,33 @@ use Matleyx\CI4P\Models\Mt_CrudModel;
 class Mt_crud extends BaseController
 {
 
-    //use \CodeIgniter\API\ResponseTrait;
+	public function index()
+	{
+		helper('form');
+		$model          = new Mt_CrudModel();
+		$data['result']    = $model->geta();
+		$data['fields'] = $array          = json_decode(json_encode($model->getb()), true);
+		$data['keys']   = $array          = json_decode(json_encode($model->getc()), true);
+		$fork           = $model->getd();
+		$data['fork']   = $array          = json_decode(json_encode($fork), true);
+		return view('Matleyx\CI4P\Views\crud\crud', $data);
+	}
 
-    public function index()
-        {
-        $model          = new Mt_CrudModel();
-        $data['tab']    = $model->geta();
-        $data['fields'] = $array          = json_decode(json_encode($model->getb()), true);
-        $data['keys']   = $array          = json_decode(json_encode($model->getc()), true);
-        $fork           = $model->getd();
-        $data['fork']   = $array          = json_decode(json_encode($fork), true);
-        return view('Matleyx\CI4P\Views\crud\crud', $data);
-        }
+	public function crudgen()
+	{
+		if ($this->request->getPost())
+		{
 
+			$tab = $this->request->getPost('tname').'1';
+			$tab2 = $this->request->getPost('tablename').'a';
+
+
+			echo $tab;
+			echo $tab2;
+
+		}
+		else
+		{
+			echo 'nulla';}
+	}
 }
--- src/Views/crud/crud.php
+++ src/Views/crud/crud.php
@@ -1,11 +1,65 @@
 <?php $this->extend('layouts/main'); ?>
 
 <?php $this->section('content'); ?>
-<h1>Crud</h1>
-<pre>
-<?php print_r($tab); ?>
-<?php print_r($fields); ?>
-<?php print_r($keys); ?>
-<?php print_r($fork); ?>
-</pre>
+<h1>
+	Cruds
+</h1>
+<div class="row">
+	<div class="col-md-6">
+		<?php echo form_open('mt_crud/crudgen',array("class"=>"form-horizontal")); ?>
+
+		<div class="row">
+			<div class="form-group">
+				<label for="tipo" class="col-md-2 control-label">
+					Tabella
+				</label>
+				<div class="col-md-4">
+					<select id="table_name" name="tname" class="form-control">
+						<?php
+						foreach ($result as $key=>$value)
+						{ ?>
+						<option value="<?php echo $key; ?>">
+							<?php echo $value; ?>
+						</option>
+						<?php } ?>
+					</select>
+				</div>
+			</div>
+			<div class="form-group">
+				<label for="data" class="col-md-2 control-label">
+					Controller
+				</label>
+				<div class="col-md-4">
+					<input type="text" name="cname" class="form-control" id="cname" />
+				</div>
+			</div>
+			<div class="form-group">
+				<label for="data" class="col-md-2 control-label">
+					Nome record
+				</label>
+				<div class="col-md-4">
+					<input type="text" name="fname" class="form-control" id="fname" />
+				</div>
+			</div>
+			<div class="form-group">
+				<label for="data" class="col-md-2 control-label">
+					Campo Obsolescenza
+				</label>
+				<div class="col-md-4">
+					<input type="text" name="obsofield" class="form-control" id="obsofield" />
+				</div>
+			</div>
+			<div class="form-group">
+				<div class="col-sm-offset-2 col-sm-10">
+					<button type="submit" name="download" class="btn btn-success">
+						Download
+					</button>
+				</div>
+			</div>
+		</div>
+
+		<?php echo form_close(); ?>
+
+	</div>
+</div>
 <?php $this->endSection(); ?>
