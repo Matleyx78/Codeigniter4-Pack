84af86cccaa4eb75446b1335da558be516bbfb09
Matleyx78 <matmat78@hotmail.it>
2020-12-07 17:53:27 +0100

    Update

--- src/Controllers/Mt_test.php
+++ src/Controllers/Mt_test.php
@@ -10,50 +10,56 @@ use Mpdf\Mpdf;
 
 class Mt_test extends BaseController
 {
-	// available to GET requests only
-	public function index()
-	{
-		helper('date');
-		$cal = new Calendario();
-		if ( $this->ionAuth->loggedIn() )
-		{
-			$data['ris'] = 'si';
-		}
-		else
-		{
-			$data['ris'] = 'no';}
-		$data['giorno1'] = now();
-		$data['mysql'] = $cal->primo_del_mese();
-		$data['ris2'] = mysql_to_human1($data['mysql']);
 
-		return view('Matleyx\CI4P\Views\test\testview',$data);
-	}
-	public function mt_exc()
-	{
-		$spreadsheet = new Spreadsheet();
-		$sheet = $spreadsheet->getActiveSheet();
-		$sheet->setCellValue('A1', 'Hello World !');
+    // available to GET requests only
+    public function index()
+        {
+        helper('date');
+        $cal = new Calendario();
+        if ($this->ionAuth->loggedIn())
+            {
+            $data['ris'] = 'si';
+            }
+        else
+            {
+            $data['ris'] = 'no';
+        }
+        $data['giorno1'] = now();
+        $data['mysql']   = $cal->primo_del_mese();
+        $data['ris2']    = mysql_to_human1($data['mysql']);
 
-		$writer = new Xlsx($spreadsheet);
-		$writer->save('../writable/hello world.xlsx');
-	}
-	public function mt_pdf()
-	{
-		try
-		{
-			$mpdf = new \Mpdf\Mpdf();
+        return view('Matleyx\CI4P\Views\test\testview', $data);
+        }
 
-			$mpdf->debug = true;
-			$mpdf->WriteHTML('Hello World');
-			ob_end_clean();
-			//return $mpdf->Output('filename.pdf', 'I');
-			return redirect()->to($mpdf->Output('filename.pdf', 'I'));
-		}
-		catch (\Mpdf\MpdfException $e)
-		{
-			// Note: safer fully qualified exception name used for catch
-			// Process the exception, log, print etc.
-			echo $e->getMessage();
-		}
-	}
-}?>
+    public function mt_exc()
+        {
+        $spreadsheet = new Spreadsheet();
+        $sheet       = $spreadsheet->getActiveSheet();
+        $sheet->setCellValue('A1', 'Hello World !');
+        $fileName    = 'data.xlsx';
+        $writer      = new Xlsx($spreadsheet);
+        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        header('Content-Disposition: attachment; filename="' . urlencode($fileName) . '"');
+        $writer->save('php://output');
+        }
+
+    public function mt_pdf()
+        {
+        try
+            {
+            $mpdf = new \Mpdf\Mpdf();
+
+            $mpdf->debug = true;
+            $mpdf->WriteHTML('Hello World');
+            ob_end_clean();
+            //return $mpdf->Output('filename.pdf', 'I');
+            return redirect()->to($mpdf->Output('filename.pdf', 'I'));
+            } catch (\Mpdf\MpdfException $e)
+            {
+            // Note: safer fully qualified exception name used for catch
+            // Process the exception, log, print etc.
+            echo $e->getMessage();
+            }
+        }
+
+}
