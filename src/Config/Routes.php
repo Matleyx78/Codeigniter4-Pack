37aa789494edc98e58adae86a01b07e3ea6be812
Matleyx78 <matmat78@hotmail.it>
2020-12-07 17:41:39 +0100

    Update

--- src/Config/Routes.php
+++ src/Config/Routes.php
@@ -10,5 +10,7 @@ $routes->group('mt_crud', ['namespace' => 'Matleyx\CI4P\Controllers'], function 
 $routes->group('mt_test', ['namespace' => 'Matleyx\CI4P\Controllers'], function ($routes)
 {
 	$routes->get('/', 'Mt_test::index');
+	$routes->get('mt_exc', 'Mt_test::mt_exc');
+	$routes->get('mt_pdf', 'Mt_test::mt_pdf');
 
 });
--- src/Controllers/Mt_test.php
+++ src/Controllers/Mt_test.php
@@ -4,25 +4,56 @@ namespace Matleyx\CI4P\Controllers;
 
 use App\Controllers\BaseController;
 use Matleyx\CI4P\Libraries\Calendario;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use Mpdf\Mpdf;
 
 class Mt_test extends BaseController
 {
-    // available to GET requests only
-    public function index()
-    {
+	// available to GET requests only
+	public function index()
+	{
 		helper('date');
 		$cal = new Calendario();
 		if ( $this->ionAuth->loggedIn() )
-			{
-				$data['ris'] = 'si';
-			}
-			else
-			{
-				$data['ris'] = 'no';}
+		{
+			$data['ris'] = 'si';
+		}
+		else
+		{
+			$data['ris'] = 'no';}
 		$data['giorno1'] = now();
 		$data['mysql'] = $cal->primo_del_mese();
 		$data['ris2'] = mysql_to_human1($data['mysql']);
-		
-        return view('Matleyx\CI4P\Views\test\testview',$data);
-    }
-} 
+
+		return view('Matleyx\CI4P\Views\test\testview',$data);
+	}
+	public function mt_exc()
+	{
+		$spreadsheet = new Spreadsheet();
+		$sheet = $spreadsheet->getActiveSheet();
+		$sheet->setCellValue('A1', 'Hello World !');
+
+		$writer = new Xlsx($spreadsheet);
+		$writer->save('../writable/hello world.xlsx');
+	}
+	public function mt_pdf()
+	{
+		try
+		{
+			$mpdf = new \Mpdf\Mpdf();
+
+			$mpdf->debug = true;
+			$mpdf->WriteHTML('Hello World');
+			ob_end_clean();
+			//return $mpdf->Output('filename.pdf', 'I');
+			return redirect()->to($mpdf->Output('filename.pdf', 'I'));
+		}
+		catch (\Mpdf\MpdfException $e)
+		{
+			// Note: safer fully qualified exception name used for catch
+			// Process the exception, log, print etc.
+			echo $e->getMessage();
+		}
+	}
+}?>
