f90e7c2b84c69b7a48b08eeba56e2d96dfe7252f
Matleyx78 <matmat78@hotmail.it>
2020-12-22 11:03:55 +0100

    Update Mt_crud.php

--- src/Controllers/Mt_crud.php
+++ src/Controllers/Mt_crud.php
@@ -8,48 +8,110 @@ use PhpZip\ZipFile;
 
 class Mt_crud extends BaseController
 {
+	    protected $model;
+    protected $table_name;
+    protected $primary_key;
+    protected $controller_name;
+    protected $record_name;
+    protected $softdeleted_enable;
+    protected $timestamp_enable;
+    protected $returntype;
 
-	public function index()
-	{
-		helper('form');
-		$model          = new Mt_CrudModel();
-		$table='cartellini_produzione';
-		$data['result'] = $model->getAllTables();
-		return view('Matleyx\CI4P\Views\crud\crud', $data);
-	}
+    public function __construct()
+        {
+        helper('form');
+        $this->model = new Mt_CrudModel();
+        }
 
-	public function crudgen()
-	{
-		if ($this->request->getMethod() === 'post')
-		{
-			$incoming['tname']     = $this->request->getPost('tname');
-			$incoming['cname']     = $this->request->getPost('cname');
-			$incoming['fname']     = $this->request->getPost('fname');
-			$incoming['obsofield'] = $this->request->getPost('obsofield');
-			$incoming['funz']      = $this->build_model($incoming['cname']);
-			$nomefile              = 'provanomefile.php';
-			$zipFile               = new \PhpZip\ZipFile();
-			try
-			{
-				$zipFile
-				->addFromString($nomefile, $incoming['funz']) // add an entry from the string
-				->outputAsAttachment($incoming['tname'] . '.zip'); // output to the browser without saving to a file
-			}
-			catch (\PhpZip\Exception\ZipException $e)
-			{
-				// handle exception
-			}
-			finally
-			{
-				$zipFile->close();
-			}
-		}
-		else
-		{
-			return redirect()->route('mt_crud');
-		}
-	}
+    public function index()
+        {
+        helper('form');
+        //$model          = new CrudtestModel();
+        $table          = 'cartellini_produzione';
+        $data['result'] = $this->model->getAllTables();
+        ret
 
+    public function crudgen()
+        {
+        if ( $this->request->getMethod() === 'post' )
+            {
+            $incoming['tname']     = $this->request->getPost('tname');
+            $incoming['cname']     = $this->request->getPost('cname');
+            $incoming['fname']     = $this->request->getPost('fname');
+            $incoming['obsofield'] = $this->request->getPost('obsofield');
+            $incoming['fields']    = $this->model->getAllFields($incoming['tname']);
+            $incoming['prk']       = $this->model->getAllIndex($incoming['tname']);
+            $incoming['fk']        = $this->model->getAllFK($incoming['tname']);
+
+            $data = $incoming;
+
+            return view('Matleyx\CI4P\Views\crud\viewtable', $data);
+
+//<<<<<<< HEAD
+//			$incoming['funz']      = $this->build_model($incoming['cname']);
+//			$nomefile              = 'provanomefile.php';
+//			$zipFile               = new \PhpZip\ZipFile();
+//			try
+//			{
+//				$zipFile
+//				->addFromString($nomefile, $incoming['funz']) // add an entry from the string
+//				->outputAsAttachment($incoming['tname'] . '.zip'); // output to the browser without saving to a file
+//			}
+//			catch (\PhpZip\Exception\ZipException $e)
+//			{
+//				// handle exception
+//			}
+//			finally
+//			{
+//				$zipFile->close();
+//			}
+            }
+        else
+            {
+            return redirect()->route('mt_crud');
+            }
+        }
+
+    public function test()
+        {
+        if ( $this->request->getMethod() === 'post' )
+            {
+            $incoming['tname']     = $this->request->getPost('tname');
+            $incoming['cname']     = $this->request->getPost('cname');
+            $incoming['fname']     = $this->request->getPost('fname');
+            $incoming['obsofield'] = $this->request->getPost('obsofield');
+            $incoming['fields']    = $this->model->getAllFields($incoming['tname']);
+            $incoming['prk']       = $this->model->getAllIndex($incoming['tname']);
+            $incoming['fk']        = $this->model->getAllFK($incoming['tname']);
+
+            $data = $incoming;
+
+            return view('Matleyx\CI4P\Views\crud\viewtable', $data);
+
+//			$incoming['funz']      = $this->build_model($incoming['cname']);
+//			$nomefile              = 'provanomefile.php';
+//			$zipFile               = new \PhpZip\ZipFile();
+//			try
+//			{
+//				$zipFile
+//				->addFromString($nomefile, $incoming['funz']) // add an entry from the string
+//				->outputAsAttachment($incoming['tname'] . '.zip'); // output to the browser without saving to a file
+//			}
+//			catch (\PhpZip\Exception\ZipException $e)
+//			{
+//				// handle exception
+//			}
+//			finally
+//			{
+//				$zipFile->close();
+//			}
+            }
+        else
+            {
+            return redirect()->route('mt_crud');
+            }
+        }	    
+	    
 	protected function build_model()
 	{
 		$file_a = '<?php namespace App\Models;
