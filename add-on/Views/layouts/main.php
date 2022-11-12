5fe56a0e3e15e5945cd9e3ad0f54f6a157f0880a
Matleyx78 <matmat78@hotmail.it>
2022-11-12 10:09:55 +0100

    Update main.php

--- add-on/Views/layouts/main.php
+++ add-on/Views/layouts/main.php
@@ -55,7 +55,7 @@ if ($loggedin)
 }
 else
 {
-	$part2 = "                          <li>".anchor('auth/login', ' Login','><span class="glyphicon glyphicon-log-out"></span')."</li>
+	$part2 = "                          <li>".anchor('login', ' Login','><span class="glyphicon glyphicon-log-out"></span')."</li>
 ";
 };
 
