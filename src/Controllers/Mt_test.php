13841ffcd38249428e4e4e5881416dca47665324
Matleyx78 <matmat78@hotmail.it>
2023-01-25 15:55:28 +0100

    Update Mt-test

--- src/Controllers/Mt_test.php
+++ src/Controllers/Mt_test.php
@@ -10,72 +10,55 @@ use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 use Mpdf\Mpdf;
 
 class Mt_test extends BaseController
-    {
+{
 
     // available to GET requests only
     public function index()
-        {
+    {
         helper('date');
 
         $cal = new Calendario();
         $giorno = '2021-03-15 00:00:00';
         $data['giorno1'] = now();
-        $data['mysql']   = $cal->primo_del_mese($giorno);
+        $data['mysql'] = $cal->primo_del_mese($giorno);
         //var_dump($data['mysql']);
         //$data['ris2']    = mysql_to_human1($data['mysql']);
 
         return view('Matleyx\CI4P\Views\test\testview', $data);
-        }
+    }
 
     public function adhoc()
-        {
+    {
         helper('date');
         $model = new Mt_TestModel();
         $data['result'] = $model->getadhoc();
 
         return view('Matleyx\CI4P\Views\test\testview', $data);
-        }
+    }
 
     public function mt_exc()
-        {
+    {
         $spreadsheet = new Spreadsheet();
-        $sheet       = $spreadsheet->getActiveSheet();
+        $sheet = $spreadsheet->getActiveSheet();
         $sheet->setCellValue('A1', 'Hello World !');
-        $fileName    = 'data.xlsx';
-        $writer      = new Xlsx($spreadsheet);
+        $fileName = 'data.xlsx';
+        ob_clean();
+        $writer = new Xlsx($spreadsheet);
         header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
         header('Content-Disposition: attachment; filename="' . urlencode($fileName) . '"');
         $writer->save('php://output');
-        }
-
-    public function mt_pdf()
-        {
-        try
-            {
-            $mpdf = new \Mpdf\Mpdf();
-
-            $mpdf->debug = true;
-            $mpdf->WriteHTML('Hello World');
-            //ob_end_clean();
-            //return $mpdf->Output('filename.pdf', 'I');
-            return redirect()->to($mpdf->Output('filename.pdf', 'I'));
-            } catch (\Mpdf\MpdfException $e)
-            {
-            // Note: safer fully qualified exception name used for catch
-            // Process the exception, log, print etc.
-            echo $e->getMessage();
-            }
-        }
+        exit();
+    }
 
     public function tedata($fr = '')
+    {
+        if (!isset($fr))
         {
-        if (! isset($fr) )
-            {
             $fr = '78';
-            }
+        }
         $data['ris'] = $fr;
 
         return view('Matleyx\CI4P\Views\test\testview', $data);
-        }
-
     }
+
+}
